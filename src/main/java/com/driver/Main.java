cfabeccaca888d8bb44853001412ecc83c502951
dukevaishnav1000 <dukevaishnav3853@gmail.com>
2022-12-17 19:03:42 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+
+        //obj.name="rikku";
+        //System.out.println(obj.name);
+        obj.setter("rikku");
+        System.out.println(obj.getter());
+    }
 }
